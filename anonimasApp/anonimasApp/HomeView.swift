8f47de5706a5888d00755d163242d4c4b957d1e1
Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>
2024-07-15 14:38:32 +0900

    HomeScreen追加実装
    Co-authored-by: Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>

--- anonimasApp/anonimasApp/HomeView.swift
+++ anonimasApp/anonimasApp/HomeView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var isPresented: Bool = false
-    
+
     var body: some View {
         VStack {
             ZStack {
@@ -25,14 +25,55 @@ struct HomeView: View {
                     }
                     .offset(CGSize(width: 0, height: -120))
                     .shadow(radius: 2, x: 0, y: 5)
-                    Text("Your Stats")
-                        .offset(CGSize(width: -120, height: -150))
-                        .foregroundColor(.black)
-                        .font(.custom("SF-Pro", size: 22))
-                    HStack(spacing: 13) {
+                    ScrollView {
+                        Text("Your Stats")
+                            .offset(CGSize(width: -120, height: 0))
+                            .foregroundColor(.black)
+                            .font(.custom("SF-Pro", size: 22))
+                        HStack(spacing: 13) {
+                            ZStack(alignment:.leading) {
+                                Rectangle()
+                                    .frame(width: 164, height: 114)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(8)
+                                    .shadow(radius: 5,y: 5)
+                                VStack(alignment:.leading, spacing: 13) {
+                                    Text("1,234")
+                                        .font(.title)
+                                        .bold()
+                                        .foregroundColor(.green)
+                                    Text("Sefety Score")
+                                        .font(.custom("SF-Pro", size: 16))
+                                        .foregroundColor(.gray)
+                                }
+                                .offset(CGSize(width: 13.0, height: 0))
+                            }
+                            ZStack(alignment:.leading) {
+                                Rectangle()
+                                    .frame(width: 164, height: 114)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(8)
+                                    .shadow(radius: 5,y: 5)
+                                VStack(alignment:.leading, spacing: 13) {
+                                    Text("1,329,234")
+                                        .font(.title)
+                                        .bold()
+                                        .foregroundColor(.blue)
+                                    Text("Team Point")
+                                        .font(.custom("SF-Pro", size: 16))
+                                        .foregroundColor(.gray)
+                                }
+                                .offset(CGSize(width: 13.0, height: 0))
+                            }
+                        }
+                        .offset(CGSize(width: 0, height: 0))
+                        Text("Recently acquired flags")
+                            .font(.custom("SF-Pro", size: 22))
+                            .foregroundColor(.black)
+                            .offset(CGSize(width: -55, height: 0))
                         ZStack(alignment:.leading) {
                             Rectangle()
-                                .frame(width: 164, height: 114)
+                                .frame(width: 346, height: 99)
                                 .foregroundColor(.white)
                                 .cornerRadius(8)
                                 .shadow(radius: 5,y: 5)
@@ -47,30 +88,33 @@ struct HomeView: View {
                             }
                             .offset(CGSize(width: 13.0, height: 0))
                         }
+                        .offset(CGSize(width: 0, height: 0))
+                        Text("Friend Challenge")
+                            .font(.custom("SF-Pro", size: 22))
+                            .foregroundColor(.black)
+                            .offset(CGSize(width: -85, height: 0))
                         ZStack(alignment:.leading) {
                             Rectangle()
-                                .frame(width: 164, height: 114)
+                                .frame(width: 346, height: 126)
                                 .foregroundColor(.white)
                                 .cornerRadius(8)
                                 .shadow(radius: 5,y: 5)
                             VStack(alignment:.leading, spacing: 13) {
-                                Text("1,329,234")
+                                Text("1,234")
                                     .font(.title)
                                     .bold()
-                                    .foregroundColor(.blue)
-                                Text("Team Point")
+                                    .foregroundColor(.green)
+                                Text("Sefety Score")
                                     .font(.custom("SF-Pro", size: 16))
                                     .foregroundColor(.gray)
                             }
                             .offset(CGSize(width: 13.0, height: 0))
                         }
+                        .offset(CGSize(width: 0, height: 0))
                     }
-                    .offset(CGSize(width: 0, height: -150))
-                    Text("Flags")
-                        .font(.custom("SF-Pro", size: 22))
-                        .foregroundColor(.black)
-                        .offset(CGSize(width: -140, height: -140))
+                    .offset(CGSize(width: 0, height: -165))
                 }
+                .offset(CGSize(width: 0, height: 160))
             }
         }
         .fullScreenCover(isPresented: $isPresented) {
