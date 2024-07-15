d34fbc0f5edc84dcd64091d28a861bf950c6e1e9
taku <takuya_hiraoka@mail.toyota.co.jp>
2024-07-16 01:13:26 +0900

    [frontend][add info]: metaデータを新たに加えて完全版アプリを表示できるようにしたいです

--- anonimasfrontend/src/components/TouchDownConnectPage.tsx
+++ anonimasfrontend/src/components/TouchDownConnectPage.tsx
@@ -6,14 +6,21 @@ export default function TouchDownConnectPage() {
         <>
             <div className="container">this is touch Down Swarm Page!!!</div>
             <a href="https://apps.apple.com/jp/app/touchdownswarm/id6532610918?itscg=30200&amp;itsct=apps_box_appicon"
-               style="width: 170px; height: 170px; border-radius: 22%; overflow: hidden; display: inline-block; vertical-align: middle;"><img
-                src="https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/e3/15/05/e3150511-e2e3-5e1b-1a8b-3ab87c36c619/AppIcon-0-0-1x_U007epad-0-0-85-220.png/540x540bb.jpg"
-                alt="TouchDownSwarm"
-                style="width: 170px; height: 170px; border-radius: 22%; overflow: hidden; display: inline-block; vertical-align: middle;"/></a>
+               style={{width: '170px', height: '170px', borderRadius: '22%', overflow: 'hidden', display: 'inline-block', verticalAlign: 'middle'}}>
+                <img
+                    src="https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/e3/15/05/e3150511-e2e3-5e1b-1a8b-3ab87c36c619/AppIcon-0-0-1x_U007epad-0-0-85-220.png/540x540bb.jpg"
+                    alt="TouchDownSwarm"
+                    style={{width: '170px', height: '170px', borderRadius: '22%', overflow: 'hidden', display: 'inline-block', verticalAlign: 'middle'}}
+                />
+            </a>
             <a href="https://apps.apple.com/jp/app/touchdownswarm/id6532610918?itsct=apps_box_badge&amp;itscg=30200"
-               style="display: inline-block; overflow: hidden; border-radius: 13px; width: 250px; height: 83px;"><img
-                src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/ja-jp?size=250x83&amp;releaseDate=1720742400"
-                alt="Download on the App Store" style="border-radius: 13px; width: 250px; height: 83px;"/></a>
+               style={{display: 'inline-block', overflow: 'hidden', borderRadius: '13px', width: '250px', height: '83px'}}>
+                <img
+                    src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/ja-jp?size=250x83&amp;releaseDate=1720742400"
+                    alt="Download on the App Store"
+                    style={{borderRadius: '13px', width: '250px', height: '83px'}}
+                />
+            </a>
             <img src="https://tools-qr-production.s3.amazonaws.com/output/apple-toolbox/88258c7b30d9b2face3f048e1dfe47a9/2cce712baf2f8cde90719941b96f8c54.png"/>
         </>
     );
